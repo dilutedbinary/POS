f8ef4218660b99b56f51872e64b7b8a1aadd2077
Owner <Owner@idea-PC>
2016-04-13 23:49:18 -0400

    roll tide

--- src/pointofsalesystem/LoginController.java
+++ src/pointofsalesystem/LoginController.java
@@ -38,7 +38,6 @@ public class LoginController {
     
     private void switchToPOS(String id, String pass){
         gui.setVisible(false);
-        System.out.println(id);
         PointOfSaleController pos = new PointOfSaleController(id, pass,model);
         gui = null;
     }
