daca801e6616b17a320e9efd09f16ca0d58c0810
Billy DeLucia <wild188@gmail.com>
2016-04-13 22:50:35 -0400

    Blacks on blonds... I'm the blond

--- src/pointofsalesystem/PointOfSaleController.java
+++ src/pointofsalesystem/PointOfSaleController.java
@@ -112,7 +112,7 @@ public class PointOfSaleController {
             String transaction = "";
             int typeID = (Integer)newTable[i].getC(); 
             
-            //System.out.println("balls: "+typeID);
+            System.out.println("balls2: "+typeID);
             
             if(typeID == 0){
                 //sale
